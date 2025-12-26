31f8cc9a26fcd09ebac1c21e798e5b0bf53a6949
DinhManh203 <dinhmanhoke203@gmail.com>
2025-12-26 14:29:18 +0700

    update

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -13,12 +13,12 @@ const Heading = () => {
     return (
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-                Ý tưởng, tài liệu, & kế hoạch của bạn. Được thống nhất. Chào mừng đến với{" "}
+                Ý tưởng, tài liệu & kế hoạch của bạn được thống nhất. Chào mừng đến với{" "}
                 <span className="underline">MiNote</span>
             </h1>
             <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-                MiNote là không gian làm việc được kết nối<br />
-                công việc tốt hơn, nhanh hơn.
+                MiNote chứa không gian làm việc<br />
+                tốt hơn, nhanh hơn.
             </h3>
             {isLoading && (
                 <div className="w-full flex items-center justify-center">
